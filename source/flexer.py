4d67f94226cb985eaa1e4b05e9a1462976c09838
Sami Bendou <samidahoux@gmail.com>
2021-04-15 18:31:22 +0200

    minor fixing

--- source/flexer.py
+++ source/flexer.py
@@ -11,229 +11,21 @@ class FuncLexer(RegexLexer):
     filenames = ['*.func', '*.f']
 
     IDENT = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    LINK = rf'{{IDENT}}'
-    IC = r'#.*\n'
-    WS = r'\t|\n| '
+    IDENT_SIMPLE = r'([a-zA-Z_][a-zA-Z0-9_]*)(\s*)(\:)'
+    IDENT_STRUCT = r'([a-zA-Z_][a-zA-Z0-9_]*)(\s*)(\{)'
+    IDENT_DOTED = r'([a-zA-Z_][a-zA-Z0-9_]*)(\s*)(\.)'
+    IDENT_PARAM = r'([a-zA-Z_][a-zA-Z0-9_]*)(\s*)(\()'
+    IDENT_ALGEBRA = r'([a-zA-Z_][a-zA-Z0-9_]*)(\s*)(<)'
+
     BUILTINS = words(('boolean', 'string', 'natural', 'integer',
-                     'rational', 'real', 'complex', 'any', 'empty'))
+                      'rational', 'real', 'complex', 'any', 'empty'))
 
     tokens = {
-        'root': [
-            (WS, Escape),
-
-            (IC, Comment.Single),
-
-            (r'"""', Comment.Multiline, 'comment'),
-
-            (rf'({IDENT})?(\')', bygroups(
-                String.Affix,
-                String.Delimiter),
-                'string'),
-
-            (rf'(define)({WS})+(constant)?({WS})*(input|output|buffer)?({WS})*({IDENT})({WS})*(;)',
-             bygroups(
-                 Keyword,
-                 Escape,
-                 Keyword,
-                 Escape,
-                 Keyword,
-                 Escape,
-                 Name.Class,
-                 Escape,
-                 Punctuation)),
-
-            (rf'(define)({WS})+(constant)?({WS})*(input|output|buffer)?({WS})*({IDENT})({WS})*(\:)',
-             bygroups(
-                 Keyword,
-                 Escape,
-                 Keyword,
-                 Escape,
-                 Keyword,
-                 Escape,
-                 Name.Class,
-                 Escape,
-                 Punctuation),
-             'constraint'),
-
-            (rf'(define)({WS})+(constant)?({WS})*(input|output|buffer)?({WS})*({IDENT})({WS})*(\{{)',
-             bygroups(
-                 Keyword,
-                 Escape,
-                 Keyword,
-                 Escape,
-                 Keyword,
-                 Escape,
-                 Name.Class,
-                 Escape,
-                 Punctuation),
-             'structured'),
-
-            (rf'(block)({WS})+({IDENT})({WS})*',
-             bygroups(
-                 Keyword,
-                 Escape,
-                 Name.Namespace,
-                 Escape),
-             'block'),
-
-
-            (rf'(import)({WS})+({IDENT})*',
-             bygroups(
-                 Keyword,
-                 Escape,
-                 Name.Namespace)),
-
-            (rf'(require)({WS})+({IDENT})({WS})+(on)({WS})+({IDENT})({WS})*(\:)',
-             bygroups(
-                 Keyword,
-                 Escape,
-                 Name.Function,
-                 Escape,
-                 Keyword,
-                 Escape,
-                 Name.Variable,
-                 Escape,
-                 Punctuation),
-             'constraint'),
-
-            (rf'(function)({WS})+({IDENT})', bygroups(Keyword,
-             Escape, Name.Function), 'function'),
-
-            include('number'),
-            include('boolean')
-        ],
-
-        'function': [
-            (rf'\(', Punctuation, 'parameter'),
-
-        ],
-
-        'parameter': [
-            (WS, Escape),
-            (rf'({IDENT})({WS})*(\:)', bygroups(Name.Variable,
-             Escape, Punctuation), 'constraint'),
-            (rf'(\))({WS})*(\:)', bygroups(Punctuation,
-                                           Escape, Punctuation), ('#pop', 'constraint')),
-            (rf'\)', Punctuation, '#pop'),
-        ],
-
-        'block': [
-            (r'\{', Punctuation, '#push'),
-            include('root'),
-            (r'\}', Punctuation, '#pop')
-        ],
-
-        'comment': [
-            (r'[^"""\{\@]', Comment.Multiline),
-            (r'\{', Punctuation, 'link'),
-            (rf'\@{IDENT}', Comment.Preproc),
-            (r'"""', Comment.Multiline, '#pop')
-        ],
-
-        'structured': [
-            (WS, Escape),
-            (r'"""', Comment.Multiline, 'comment'),
-            (rf'({IDENT})({WS})*(\:)',
-             bygroups(Name.Property, Escape, Punctuation),
-             'constraint'),
-            (IC, Comment.Single),
-            (r'\:', Punctuation, 'constraint'),
-            (r'\{', Punctuation),
-            (r'\}', Punctuation, '#pop')
-        ],
-
-        'constraint': [
-            (WS, Escape),
-            (BUILTINS, Name.Builtin),
-            (rf'({IDENT})(<)', bygroups(
-                Name.Variable.Decorator, Punctuation), ('algebraical-field', 'algebraical-dimension')),
-            (rf'{IDENT}', Name.Variable.Class),
-            (rf'&&|\|\||\*|\*\*', Operator),
-            (r'\[\[', Punctuation, 'maths'),
-            (r'\{\{', Punctuation, 'algorithm'),
-            (r'\(\(', Punctuation, 'enumerated'),
-            (r'(<<|>>)', Punctuation, 'interval'),
-            (r'""', String.Doc, 'semantic'),
-            (r';', Punctuation, '#pop')
-        ],
-
-        'algebraical-dimension': [
-            (WS, Escape),
-            include('number'),
-            (r',', Punctuation),
-            (r'>', Punctuation, '#pop')
-        ],
-
-        'algebraical-field': [
-            (BUILTINS, Name.Builtin),
-            (r'\(', Punctuation),
-            (r'\)', Punctuation, '#pop')
-        ],
-
-        'interval': [
-            include('number'),
-            (r'\:', Punctuation),
-            (r'(<<|>>)', Punctuation, '#pop'),
-        ],
-
-        'enumerated': [
-            (WS, Escape),
-            (r',', Punctuation),
-            include('number'),
-            (rf'({IDENT})?(\')', bygroups(
-                String.Affix,
-                String.Delimiter),
-                'string'),
-            (r'\)\)', Punctuation, '#pop')
-        ],
-
-        'algorithm': [
-            (WS, Escape),
-            include('algorithm-expr'),
-            (r'\}\}', Punctuation, '#pop')
-        ],
-
-        'algorithm-expr': [
-            (WS, Escape),
-            include('number'),
-            (words(('let', 'if', 'else', 'for', 'in')), Keyword),
-            (words((';', '(', ')', '.', '{', '}', ',')), Punctuation),
-            (r';|\(|\)|\{|\}', Punctuation),
-            (rf'({IDENT})', Name.Variable),
-            (words(('>','=','<','!=','>=','<=','+','/','-','*','%','@','^')), Operator),
-        ],
-
-        'maths': [
-            include('maths-expr'),
-            (r'\]\]', Punctuation, '#pop')
-        ],
-
-        'maths-expr': [
-            (WS, Escape),
-            include('number'),
-            (words(('let', 'exists', 'for', 'in')), Keyword),
-            (words((';', '(', ')', '.', ',')), Punctuation),
-            (rf'({IDENT})', Name.Variable),
-            (words(('>','=','<','!=','>=','<=','+','/','-','*','%','@','^')), Operator),
-        ],
-
-        'semantic': [
-            (r'[^\{""]', String.Doc),
-            (r'\{', Punctuation, 'link'),
-            (r'""', String.Doc, '#pop'),
-        ],
 
         'link': [
-            (WS, Escape),
             (r'\.', Punctuation),
-            (rf'{IDENT}', Name.Variable),
+            (IDENT, Name.Namespace),
             (r'\}', Punctuation, '#pop')
-        ],
-
-        'string': [
-            (r'[^\'\n]', String.Delimiter),
-            (r'\\.', String.Escape),
-            (r'\'', String, '#pop')
         ],
 
         'boolean': [
@@ -245,5 +37,170 @@ class FuncLexer(RegexLexer):
             (r'(-)?([0-9]+)\.([0-9]+)(e(-)?[0-9]+)?', Number.Float),
             (r'(-)?([0-9]+)(e(-)?[0-9]+)', Number.Float),
             (r'(-)?([0-9]+)(//[0-9]+)?', Number.Integer.Long),
-        ]
+        ],
+
+        'string': [
+            (r'\\.', String.Escape),
+            (r'[^\']', String.Single),
+            (r'\'', String, '#pop')
+        ],
+
+        'base': [
+            (IDENT, Name.Variable),
+            (r'\s', Whitespace),
+            (r'#.*\n', Comment.Single),
+            ('"""', Comment.Multiline, 'comment'),
+            (r'([a-zA-Z0-9]*)(\')',
+             bygroups(String.Interpol, String.Delimiter), 'string'),
+            include('number'),
+            include('boolean'),
+        ],
+
+        'root': [
+            ('define', Keyword, 'quantity'),
+            ('block', Keyword, 'block'),
+            ('require', Keyword, 'requirement'),
+            ('import', Keyword, 'import'),
+            ('function', Keyword, 'function'),
+
+            include('base'),
+        ],
+
+        'block': [
+            (IDENT_STRUCT,
+             bygroups(Name.Namespace, Whitespace, Punctuation), '#push'),
+            (r'\}', Punctuation, '#pop'),
+            include('root'),
+        ],
+
+        'quantity': [
+            (words(('input', 'output', 'buffer', 'constant')), Keyword),
+            (IDENT_SIMPLE,
+             bygroups(Name.Property, Whitespace, Punctuation), ('#pop', 'constraint')),
+            (IDENT_STRUCT,
+             bygroups(Name.Class, Whitespace, Punctuation), ('#pop', 'quantity-structured')),
+            include('base'),
+        ],
+        'quantity-structured': [
+            (IDENT_SIMPLE,
+             bygroups(Name.Property, Whitespace, Punctuation), 'constraint'),
+            (IDENT_STRUCT,
+             bygroups(Name.Class, Whitespace, Punctuation), '#push'),
+            (r'\}', Punctuation, '#pop'),
+            include('base'),
+        ],
+
+        'requirement': [
+            (words(('on', 'derives', 'specializes', 'refines')), Keyword),
+            (IDENT_SIMPLE,
+             bygroups(Name.Property, Whitespace, Punctuation), ('#pop', 'constraint')),
+            (IDENT, Name.Function),
+            include('base'),
+        ],
+
+        'import': [
+            ('as', Keyword),
+            (IDENT_DOTED, bygroups(Name.Namespace, Punctuation)),
+            (IDENT, Name.Namespace, '#pop'),
+            include('base'),
+        ],
+
+        'function': [
+            (IDENT_PARAM,
+             bygroups(Name.Function, Whitespace, Punctuation), 'function-parameters'),
+            (r'\:', Punctuation, ('#pop', 'constraint')),
+            include('base'),
+        ],
+        'function-parameters': [
+
+            (IDENT_SIMPLE,
+             bygroups(Name.Variable, Whitespace, Punctuation), 'constraint'),
+            (r'\)', Punctuation, '#pop'),
+            include('base'),
+        ],
+
+        'comment': [
+            (r'[^"""\{\@]', Comment.Multiline),
+            (r'\{', Punctuation, 'link'),
+            (r'\@[^\s]+', Comment.Preproc),
+            ('"""', Comment.Multiline, '#pop')
+        ],
+
+        'constraint': [
+            (IDENT_ALGEBRA,
+             bygroups(Name.Variable.Decorator, Whitespace, Punctuation), ('algebra-field', 'algebra-dimension')),
+            (r'""', String.Doc, 'semantic'),
+            (r'\[\[', Punctuation, 'maths'),
+            (r'\{\{', Punctuation, 'algorithm'),
+            (r'\(\(', Punctuation, 'enumerated'),
+            (r'(<<|>>)', Punctuation, 'interval'),
+            (r';', Punctuation, '#pop'),
+            (BUILTINS, Name.Builtin),
+            (IDENT, Name.Attribute),
+            (r'&&|\|\||\*|\*\*', Operator),
+            include('base'),
+        ],
+
+        'semantic': [
+            (r'[^\{\{""]', String.Doc),
+            (r'\{', Punctuation, 'link'),
+            (r'""', String.Doc, '#pop'),
+        ],
+
+        'algebra-dimension': [
+            (r',', Punctuation),
+            (r'>', Punctuation, '#pop'),
+            include('number'),
+            include('base'),
+        ],
+        'algebra-field': [
+            (r'\)', Punctuation, '#pop'),
+            (BUILTINS, Name.Builtin),
+            (r'\(', Punctuation),
+            include('base'),
+        ],
+
+        'interval': [
+            (r'(<<|>>)', Punctuation, '#pop'),
+            (r'\:', Punctuation),
+            include('number'),
+            include('base'),
+        ],
+
+        'enumerated': [
+            (r'\)\)', Punctuation, '#pop'),
+            (r',', Punctuation),
+            include('number'),
+            include('base'),
+        ],
+
+        'algorithm': [
+            (r'\}\}', Punctuation, '#pop'),
+            include('algorithm-expr'),
+            include('base'),
+        ],
+        'algorithm-expr': [
+            (words(('let', 'if', 'else', 'for', 'in')), Keyword),
+            (words((';', '(', ')', '.', '{', '}', ',')), Punctuation),
+            (r';|\(|\)|\{|\}', Punctuation),
+            (words(('>', '=', '<', '!=', '>=', '<=', '+',
+             '/', '-', '*', '%', '@', '^')), Operator),
+            (IDENT, Name.Variable),
+            include('number'),
+            include('base'),
+        ],
+
+        'maths': [
+            include('maths-expr'),
+            (r'\]\]', Punctuation, '#pop')
+        ],
+        'maths-expr': [
+            include('base'),
+            include('number'),
+            (words(('let', 'exists', 'for', 'in')), Keyword),
+            (words((';', '(', ')', '.', ',')), Punctuation),
+            (words(('>', '=', '<', '!=', '>=', '<=', '+',
+             '/', '-', '*', '%', '@', '^')), Operator),
+            (IDENT, Name.Variable),
+        ],
     }
